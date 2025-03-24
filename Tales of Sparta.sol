c1a7b5b939f0ee86cc183311bf5632046ef3f863
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-03-24 16:14:24 -0700

    Create Tales of Sparta.sol
    Creation of Sparta on Sonic

---
+++ Tales of Sparta.sol
@@ -0,0 +1,6 @@
+// Custom contract
+// @title Tales of Sparta 
+// Original Storyboard Comic book Tales of Sparta on Sonic 
+// website: https://sparta.my | telegram: https://t.me/spartaonsonic | twitter: https://x.com/spartaonsonic
+
+pragma solidity ^0.8.18;
