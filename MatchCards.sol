8470250b7ee7349554c488bd6437ff8e375a9e86
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-06-15 18:08:27 +0100

    Create MatchCards.sol

---
+++ MatchCards.sol
@@ -0,0 +1,10 @@
+
+// File: contracts/MatchCards.sol
+// Custom contract
+// @title MatchBet GambleFi
+// Match Bet Player Cards on Matchain 
+// website: https://matchbet.my | telegram: https://t.me/matchbet | twitter: https://x.com/matchbet
+
+pragma solidity ^0.8.18;
+
+contract MatchCards is ERC721Enumerable, Ownable, ReentrancyGuard {        }
