608901af3ba8933e88426d691b998d116299f6d4
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-06-15 18:48:35 +0100

    Update erc20token.sol

--- erc20token.sol
+++ erc20token.sol
@@ -3,4 +3,14 @@ pragma solidity ^0.8.4;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract SoulOfSparta is ERC20 {}
+contract SoulOfSparta is ERC20 {
+    constructor(string memory _name, string memory _symbol) public 
+    ERC20(_name, _symbol)
+    {
+        mint();
+    }
+
+    function mint() public {
+        _mint(msg.sender, 1000000000 * 1 ether); // Minting 100 trillion tokens to the deployer
+    }
+}
