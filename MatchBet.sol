5bf51c6210402936ea09ec90066d2c5a0abaa293
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-06-15 22:40:45 +0100

    Create MatchBet.sol

---
+++ MatchBet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/ReentrancyGuard.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+
+interface IMatchBet {
+    function balanceOf(address _sender) external view returns (uint256);
+    function ownerOf(uint256 _index) external view returns (address);
+    function blacklisted(uint256 _index) external view returns (bool);
+}
