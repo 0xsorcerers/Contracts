bd5738387c826e95bae6aa808a838d27c5e5ecf9
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-06-15 18:48:17 +0100

    Create erc20token.sol

---
+++ erc20token.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract SoulOfSparta is ERC20 {}
