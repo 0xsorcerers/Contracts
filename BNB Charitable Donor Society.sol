e5826cb729b333a94d30e966d2671d1b856783c8
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-09-24 19:37:33 +0100

    Create BNB Charitable Donor Society.sol

---
+++ BNB Charitable Donor Society.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./CustomizedERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.4.0/contracts/utils/ReentrancyGuard.sol";
