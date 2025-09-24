9367967a3c4b1659d8de322aeca6a44abdb86ed1
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-09-24 19:35:13 +0100

    Create CharityDonorSocietyERC20.sol
    A few changes to transfer operations

---
+++ CustomizedERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.4.0) (token/ERC20/ERC20.sol)
+
+pragma solidity ^0.8.20;
+
+import {IERC20} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.4.0/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.4.0/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {Context} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.4.0/contracts/utils/Context.sol";
+import {IERC20Errors} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.4.0/contracts/interfaces/draft-IERC6093.sol";
