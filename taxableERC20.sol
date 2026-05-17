52e354962fd12f2877505eaa150b876e27ec3b5f
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2026-05-17 17:20:03 +0100

    Add penalty parameter to Penny contract constructor

--- taxableERC20.sol
+++ taxableERC20.sol
@@ -8,13 +8,15 @@ import "contracts/contracts/taxableInternalERC20.sol";
 
 contract Penny is ERC20, ReentrancyGuard {
     // A simple ERC20 token created with OpenZeppelin for best practices.
-    constructor(string memory _name, string memory _symbol, address _pennyDAO, address _devWallet, address _lpWallet, address _deadWallet) public 
+    constructor(string memory _name, string memory _symbol, address _pennyDAO, address _devWallet, address _lpWallet, 
+    address _deadWallet, uint256 _penalty) public 
     ERC20(_name, _symbol)
     {
         pennyDAO = _pennyDAO;
         devWallet = _devWallet;
         lpWallet = _lpWallet;
         deadWallet = _deadWallet;
+        penalty = _penalty;
        _mint(msg.sender, 10_000_000 * 1 ether); // One-time mint of full supply of 1 trillion tokens to the deployer
     }
 
